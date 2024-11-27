6f2d824a407556469d5b2c4d411411345ca67710
Semmieboy YT <semvdmeij124@gmail.com>
2024-11-27 11:39:27 +0100

    Add teams set

---
+++ !C#/C3-Schoolvoetbal-Backend/Data/Team.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C3_Schoolvoetbal_Backend.Data;
+
+public class Team(string name) {
+    public int Id { get; set; }
+
+    [MaxLength(255)]
+    public string Name { get; set; } = name;
+}
